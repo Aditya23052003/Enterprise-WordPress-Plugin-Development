bc58619f85c61efd935b344ffffa60c321d3abf4
Aditya Jadhav <98929144+Aditya23052003@users.noreply.github.com>
2024-05-17 23:05:29 +0530

    Update custom.js

--- includes/assets/js/custom.js
+++ includes/assets/js/custom.js
@@ -1,1 +1,20 @@
+jQuery(document).ready(function($) {
+    $('#my-button').on('click', function() {
+        $.ajax({
+            url: ajaxurl,
+            type: 'post',
+            data: {
+                action: 'my_custom_api_request',
+                security: my_custom_object.nonce
+            },
+            success: function(response) {
+                if (response.success) {
+                    console.log(response.data);
+                } else {
+                    console.log('API request failed');
+                }
+            }
+        });
+    });
+});
 
